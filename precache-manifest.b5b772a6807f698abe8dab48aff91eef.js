2da7bf3394ccd095bc87f28de28078c4df09d820
Sudhanshu Gautam <sgautam@wikimedia.org>
2024-09-24 18:49:42 +0530

    Updates

---
+++ precache-manifest.b5b772a6807f698abe8dab48aff91eef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dec529bb606550ced42ccec067ac9ea5",
+    "url": "/my-resume/index.html"
+  },
+  {
+    "revision": "db476a525da03cb13cca",
+    "url": "/my-resume/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0c9e6daa99fe98078868",
+    "url": "/my-resume/static/js/2.274d85ec.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/my-resume/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "db476a525da03cb13cca",
+    "url": "/my-resume/static/js/main.905e3777.chunk.js"
+  },
+  {
+    "revision": "f06329c888c1a1d7383e",
+    "url": "/my-resume/static/js/runtime~main.5046021d.js"
+  }
+]);
